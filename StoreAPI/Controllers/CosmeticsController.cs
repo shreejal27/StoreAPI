0d91c5d2b0afd627c86ef963d14d2f6adce886d5
Shreejal Maharjan <shreejal27@gmail.com>
2024-03-01 16:54:12 +0545

    added static cosmetic data and create HTTPGET

---
+++ StoreAPI/Controllers/CosmeticsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreAPI.Models;
+
+namespace StoreAPI.Controllers
+{
+    [ApiController]
+    public class CosmeticsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/cosmetics")]
+        public ActionResult <CosmeticsDto> GetCosmetics()
+        {
+            var cosmeticsToReturn = CosmeticsDataStore.Current;
+
+            if (cosmeticsToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cosmeticsToReturn);
+        }
+
+        [HttpGet]
+        [Route("api/cosmetics/getById")]
+        public ActionResult<CosmeticsDto> GetCosmeticsIndex(int id)
+        {
+            var cosmeticsToReturn = CosmeticsDataStore.Current.FirstOrDefault(c=>c.Id == id);
+
+            if (cosmeticsToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cosmeticsToReturn);
+        }
+
+        [HttpGet]
+        [Route("api/cosmetics/getByName")]
+        public ActionResult<CosmeticsDto> GetCosmeticsName(string name)
+        {
+            var cosmeticsToReturn = CosmeticsDataStore.Current.FirstOrDefault(c => c.Name == name);
+
+            if (cosmeticsToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cosmeticsToReturn);
+        }
+
+       
+    }
+}
--- StoreAPI/CosmeticsDataStore.cs
+++ StoreAPI/CosmeticsDataStore.cs
@@ -1,6 +1,35 @@
-﻿namespace StoreAPI
+﻿using System.Collections.Generic;
+using StoreAPI.Models;
+
+namespace StoreAPI
 {
     public class CosmeticsDataStore
     {
+        public static List<CosmeticsDto> Current { get; set; } = new List<CosmeticsDto>();
+
+        static CosmeticsDataStore()
+        {
+            Current = new List<CosmeticsDto>()
+            {
+                new CosmeticsDto()
+                {
+                    Id = 1,
+                    Name = "Foundation",
+                    Description = "natural looking",
+                },
+                new CosmeticsDto()
+                {
+                    Id = 2,
+                    Name = "Eye liner",
+                    Description = "eyes",
+                },
+                 new CosmeticsDto()
+                {
+                    Id = 3,
+                    Name = "Blush",
+                    Description = "colors",
+                }
+            };
+        }
     }
 }
