686d2eda8bfc6b8d34a61f3ceb1149df20130edf
Shreejal Maharjan <shreejal27@gmail.com>
2024-03-01 18:31:04 +0545

    added POST,PUT,PATCH and DELETE methods

--- StoreAPI/Controllers/CosmeticsController.cs
+++ StoreAPI/Controllers/CosmeticsController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StoreAPI.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using System.Xml.XPath;
+using Microsoft.VisualStudio.Services.WebApi.Patch;
 
 namespace StoreAPI.Controllers
 {
@@ -7,8 +10,8 @@ namespace StoreAPI.Controllers
     public class CosmeticsController : ControllerBase
     {
         [HttpGet]
-        [Route("api/cosmetics")]
-        public ActionResult <CosmeticsDto> GetCosmetics()
+        [Route("api/cosmetics/get", Name = "GetCosmetics")]
+        public ActionResult<CosmeticsDto> GetCosmetics()
         {
             var cosmeticsToReturn = CosmeticsDataStore.Current;
 
@@ -24,7 +27,7 @@ namespace StoreAPI.Controllers
         [Route("api/cosmetics/getById")]
         public ActionResult<CosmeticsDto> GetCosmeticsIndex(int id)
         {
-            var cosmeticsToReturn = CosmeticsDataStore.Current.FirstOrDefault(c=>c.Id == id);
+            var cosmeticsToReturn = CosmeticsDataStore.Current.FirstOrDefault(c => c.Id == id);
 
             if (cosmeticsToReturn == null)
             {
@@ -48,6 +51,89 @@ namespace StoreAPI.Controllers
             return Ok(cosmeticsToReturn);
         }
 
-       
+        [HttpPost]
+        [Route("api/cosmetics/post")]
+        public ActionResult<CosmeticsDto> CreateCosmetics(CosmeticsDto cosmeticsDto)
+        {
+            var cosmeticsId = CosmeticsDataStore.Current.Max(c => c.Id);
+
+            if (cosmeticsDto == null)
+            {
+                return BadRequest();
+            }
+
+            var cosmeticsToAdd = new CosmeticsDto()
+            {
+                Id = ++cosmeticsId,
+                Name = cosmeticsDto.Name,
+                Description = cosmeticsDto.Description,
+            };
+
+            CosmeticsDataStore.Current.Add(cosmeticsToAdd);
+
+            return Ok(cosmeticsToAdd);
+
+
+            //return CreatedAtRoute("GetCosmetics", cosmeticsDto);
+        }
+
+        [HttpPut]
+        [Route("api/cosmetics/put")]
+        public ActionResult<CosmeticsDto> UpdateCosmetics(int cosmeticsId, CosmeticsDto cosmeticsDto)
+        {
+            var cosmetics = CosmeticsDataStore.Current.FirstOrDefault(c => c.Id == cosmeticsId);
+            if (cosmetics == null)
+            {
+                return NotFound();
+            }
+
+            cosmetics.Name = cosmeticsDto.Name;
+            cosmetics.Description = cosmeticsDto.Description;
+
+            return NoContent();
+        }
+        //[HttpPatch]
+        //[Route("api/cosmetics/patch")]
+        //public ActionResult<CosmeticsDto> PartiallyUpdateCosmetics(int cosmeticsId, [FromBody] JsonPatchDocument<CosmeticsDto> patchDocument)
+        //{
+        //    var cosmetics = CosmeticsDataStore.Current.FirstOrDefault(c => c.Id == cosmeticsId);
+        //    if (cosmetics == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var cosmeticsToPatch = new CosmeticsDto()
+        //    {
+        //        Name = cosmetics.Name,
+        //        Description = cosmetics.Description
+        //    };
+
+        //    patchDocument.ApplyTo(cosmeticsToPatch, ModelState);
+
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    cosmetics.Name = cosmeticsToPatch.Name;
+        //    cosmetics.Description = cosmeticsToPatch.Description;
+
+        //    return NoContent();
+        //}
+
+        [HttpDelete]
+        [Route("api/cosmetics/delete")]
+        public ActionResult DeleteCosmetics(int cosmeticsId)
+        {
+            var cosmetics = CosmeticsDataStore.Current.FirstOrDefault(c => c.Id == cosmeticsId);
+            if (cosmetics == null)
+            {
+                return NotFound();
+            }
+
+            CosmeticsDataStore.Current.Remove(cosmetics);
+
+            return NoContent();
+        }
     }
 }
